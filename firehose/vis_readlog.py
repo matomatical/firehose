b90d8fd5f1c99daed9c6c94c826d6709f69eda21
Matthew Farrugia-Roberts <m@far.in.net>
2025-09-25 10:23:18 +0100

    readlog

--- firehose/vis_readlog.py
+++ firehose/vis_readlog.py
@@ -111,8 +111,7 @@ def hilbert(
             if new_titles:
                 vis = mp.hilbert(
                     data=read_vec,
-                    dotcolor=(0.0, 1.0, 1.0),
-                    bgcolor=(0.2, 0.0, 0.2),
+                    color=(0.0, 1.0, 1.0),
                 )
                 if not rendered: # first time
                     print(vis)
